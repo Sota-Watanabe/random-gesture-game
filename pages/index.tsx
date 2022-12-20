faae9dda8d45d3293e37d4eda6dc0a78f331e302
Sota-Watanabe <stronger.riceball@gmail.com>
2022-12-21 01:15:53 +0900

    テスト

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Home() {
   return (
-    <h1 className="text-1xl md:text-6xl text-yellow-400">
-      貴様！見ているなッ！
-    </h1>
+    <>
+      <h1 className="text-1xl md:text-6xl text-yellow-400">
+        貴様！見ているなッ！
+      </h1>
+      <p>fdsfa</p>
+    </>
   );
 }
