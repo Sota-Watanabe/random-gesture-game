e223e1bfc27049a7f2b1b8a825eb21d7a6ff1f9d
Sota-Watanabe <stronger.riceball@gmail.com>
2022-12-21 16:53:05 +0900

    basePath の指定削除

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-const isProd = process.env.NODE_ENV === "production";
-const prefixPath = isProd ? "/random-gesture-game" : "";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: prefixPath,
 };
 
 module.exports = nextConfig;
