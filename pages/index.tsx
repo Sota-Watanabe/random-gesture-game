40ecaadda1561e16a6b5b38860a1c8a0ff415949
Sota-Watanabe <stronger.riceball@gmail.com>
2022-12-21 12:40:20 +0900

    動作確認

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { themeList } from "public/theme-list";
 
 export default function Home() {
+  console.log(themeList);
+  const [theme, setTheme] = useState("お題");
+  const [point, setPoint] = useState(0);
   return (
-    <>
-      <h1 className="text-1xl md:text-6xl text-yellow-400">
-        貴様！見ているなッ！
-      </h1>
-      <a href="/ababa"></a>
-      <p>fdsfa</p>
-    </>
+    <div className="m-8 text-center">
+      <div>
+        <p className="mb-8">お題は？</p>
+        <p className="text-xl">{theme}</p>
+        <button className="mt-12 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          これだ！
+        </button>
+      </div>
+      <div className="flex gap-2 mt-10 justify-center">
+        <button
+          type="button"
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setPoint(point + 1)}
+        >
+          伝わった！
+        </button>
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          パス！
+        </button>
+      </div>
+      <div className="m-16">
+        <p className="m-4">獲得ポイント</p>
+        <p className="text-xl">{point}</p>
+      </div>
+    </div>
   );
 }
