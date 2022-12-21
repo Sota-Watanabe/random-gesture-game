e7caf838c30b3bcf9c6dbdde7dc308d1683161ba
Sota-Watanabe <stronger.riceball@gmail.com>
2022-12-21 12:10:30 +0900

    product 切り替え

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const isProd = process.env.NODE_ENV === "production";
+const prefixPath = isProd ? "/random-gesture-game" : "";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: "/random-gesture-game",
+  basePath: prefixPath,
 };
 
 module.exports = nextConfig;
