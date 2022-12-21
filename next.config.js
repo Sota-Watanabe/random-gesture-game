f64609bceba1f1ac9a63a8211b9cc720a6c27d39
Sota-Watanabe <stronger.riceball@gmail.com>
2022-12-21 11:27:06 +0900

    rewrite

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '',
-}
+  async rewrites() {
+    return [
+      {
+        source: "/random-gesture-game/_next/:path*",
+        destination: "/_next/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
