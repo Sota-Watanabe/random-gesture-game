986eac3c7bdd4c052ac0f595932eb17450f6696c
Sota-Watanabe <Sota-Watanabe@users.noreply.github.com>
2022-12-21 05:03:41 +0000

    deploy: 0cc6d164176703793410344e6d548e30d77be868

---
+++ _next/static/IeNBzsvClo-sRk2sWZ_Wq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d0e5b7db972070d0.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
