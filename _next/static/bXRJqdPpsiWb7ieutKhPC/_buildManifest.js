55c6bbb12e6d7b4e1700248160aae1005243e1a5
Sota-Watanabe <Sota-Watanabe@users.noreply.github.com>
2022-12-21 02:27:49 +0000

    deploy: f64609bceba1f1ac9a63a8211b9cc720a6c27d39

---
+++ _next/static/bXRJqdPpsiWb7ieutKhPC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/random-gesture-game/_next/:path*",destination:"/_next/:path*"}],fallback:[]},"/":["static/chunks/pages/index-042e1c7647a450f7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
